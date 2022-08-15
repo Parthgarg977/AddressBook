48b77d944d24ebe5166c9d7512a36703008ad9e3
parthgarg977 <parthgarg977@gmail.com>
2022-08-15 21:28:43 +0530

    UC1 1st commit

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AddressBook
+{
+    class Main_Address_book
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddContact add = new AddContact();
+            add.AddContactm();
+            Contacts contacts = add.AddContactm();
+        }
+    }
+}
